74ff51c5716dba402837fba3bfad09611045c8a4
Zim <zimtsui@gmail.com>
2019-07-08 21:16:11 +0800

    removed webpacknodeexternals

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpackNodeExternals = require('webpack-node-externals');
+// const webpackNodeExternals = require('webpack-node-externals');
 
 module.exports = {
     mode: 'development',
@@ -17,5 +17,5 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
     ],
-    externals: [webpackNodeExternals()],
+    // externals: [webpackNodeExternals()],
 };
